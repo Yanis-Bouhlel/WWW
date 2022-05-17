660c67c3a3ffc336cce5901c90c82902d492baea
Yanis-Bouhlel <yanis.bouhlel@lycee-jeanrostand.fr>
2022-05-18 00:41:10 +0200

    VERSION FINALE

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -1,11 +1,10 @@
-function SignIn()
-{
+function SignIn() {
     var inputs = document.getElementsByTagName("input");
     var checkbox = document.getElementsByClassName('form-check-input');
     var checkboxLabel = document.getElementsByClassName('form-check-label');
     var erreur = document.getElementById("valid");
     var formulaire = document.getElementById("formlr");
-    var button = document.getElementById("yanis");
+    var button = document.getElementById("btn");
     var test = document.getElementById("Signup");
     var Boutons = document.getElementById("bton");
     const arobaze = /[@]/g;
@@ -21,6 +20,8 @@ function SignIn()
     var verif = false;
     var mail = inputs[1];
     var password = inputs[2];
+    var password2 = inputs[3];
+    var status_id = 0;
 
 
     if (mail.value.match(miniscule)) //l'adresse doit comporter des miniscule
@@ -33,61 +34,74 @@ function SignIn()
                     erreur.innerHTML = "";
                     mail.style.borderColor = "green";
                     verifMail = true;
-                }
-                else {
+                } else {
                     erreur.style.color = "red";
                     erreur.innerHTML = "Veuillez taper le nom de domaine ! ";
                 }
-            }
-            else {
+            } else {
                 erreur.style.color = "red";
                 erreur.innerHTML = "Veuillez vérifier le mail ! ";
             }
-        }
-        else {
+        } else {
             erreur.style.color = "red";
             erreur.innerHTML = "Veuillez taper le @ ! ";
         }
-    }
-
-    else {
+    } else {
         mail.style.borderColor = "red";
         verifMail = false;
         erreur.style.color = "red";
         erreur.innerHTML = "Veuillez taper le mail ! ";
     }
 
-    if (password.value.match(miniscule))
-    {
-        if (password.value.match(num))
-        {
+
+    if (password.value.match(miniscule)) {
+        if (password.value.match(num)) {
             password.style.borderColor = "green";
-            verifpass = true;
+            verifPass = true;
         }
-    }
-    else
-    {
+    } else {
         password.style.borderColor = "red";
-        verifpass = false ;
+        verifPass = false;
     }
 
 
-    if (verifMail == true && verifPass == true)
-    {
-        console.log("oui");
-        var form= "email="+mail.value+"&password="+password.value; //On envoi les donnée des input au php
+
+
+
+
+    if (verifMail == true && verifPass == true ) {
+
+
+        var form = "email=" + mail.value + "&password=" + password.value+ "&method=SignIn"; //On envoi les donnée des input au php
 
         $.ajax({
-            url: "./assets/js/traitement.php",
-            dataType : "JSON",
-            data :  form,
+            url: "./assets/php/traitement.php",
+            dataType: "JSON",
+            data: form,
             method: "GET",
-            success:function(msg) {
+            success: function (msg) {
                 console.log(msg);
                 console.log(msg.messageAccount.status_id);
 
                 var statut_id = msg.messageAccount.status_id;
                 var reponse = msg.messageAccount;
+
+
+                /*   if (reponse == 1)
+                   {console.log(reponse);
+                       test.innerHTML = "";
+                       test.style.fontSize = "100px";
+                       test.innerHTML = "Bienvenue " + msg.messageAccount.email + ", nous sommes ravie de te revoir";
+                   }
+                   else if (reponse == 0)
+                   {
+                       erreur.style.color = "red";
+                       erreur.style.fontSize = "20px";
+                       erreur.innerHTML = "ERROR, le mail " + msg.messageAccount.email + " est déja utilisé";
+                   }
+                   else
+                       alert("ALERTE");*/
+
 
                 switch (statut_id) {
 
@@ -105,7 +119,6 @@ function SignIn()
                         initialisation();
                         break;
                 }
-
             }
 
 
@@ -113,13 +126,13 @@ function SignIn()
         //
 
     }
+
 }
 
 
 
 
-
-function SignUp()
+    function SignUp()
 {    var inputs = document.getElementsByTagName("input");
     var checkbox = document.getElementsByClassName('form-check-input');
     var checkboxLabel = document.getElementsByClassName('form-check-label');
@@ -139,21 +152,11 @@ function SignUp()
     var verifPass = false;
     var verifbox = false;
     var verif = false;
-    verifpass2 = false ;
+    var verifpass2 = false ;
     var mail = inputs[1];
     var password = inputs[2];
     var password2 = inputs[3];
     var status_id = 0 ;
-
-
-
-
-
-
-
-
-
-
 
 
     if (mail.value.match(miniscule)) //l'adresse doit comporter des miniscule
@@ -199,8 +202,8 @@ function SignUp()
     }
 
     else {
-        password.style.borderColor = "red";
-        password2.style.borderColor = "red";
+        password.style.borderColor = "green";
+        password2.style.borderColor = "green";
         verifpass2 = false ;
     }
 
@@ -212,7 +215,7 @@ function SignUp()
         }
         else {
             password2.style.borderColor = "red";
-            verifpass = false;
+            verifPass = false;
             erreur.style.color = "red";
             erreur.innerHTML = "Les mots de passe sont différents ! ";
 
@@ -230,60 +233,44 @@ function SignUp()
 
     if (verifMail == true && verifPass == true && verifbox == true) {
         verif = true;
+        var method = "SignUp" ;
+console.log(form);
 
-
-
-      var form= "email="+mail.value+"&password="+password.value+"&password2="; //On envoi les donnée des input au php
+        var form= "email="+mail.value+"&password="+password.value+"&password2="+password2.value+"&method="+method; //On envoi les donnée des input au php
 
         $.ajax({
-                url: "./assets/js/traitement.php",
-                dataType : "JSON",
-                data :  form,
-                method: "GET",
-                success:function(msg) {
+            url: "./assets/php/traitement.php",
+            dataType : "JSON",
+            data :  form,
+            method: "GET",
+            success:function(msg) {
                 console.log(msg);
-                    console.log(msg.messageAccount.status_id);
+                console.log(msg.messageAccount.status_id);
 
-                    var statut_id = msg.messageAccount.status_id;
-                    var reponse = msg.messageAccount;
+                var statut_id = msg.messageAccount.status_id;
+                var reponse = msg.messageAccount;
 
 
 
-                 /*   if (reponse == 1)
-                    {console.log(reponse);
-                        test.innerHTML = "";
-                        test.style.fontSize = "100px";
-                        test.innerHTML = "Bienvenue " + msg.messageAccount.email + ", nous sommes ravie de te revoir";
-                    }
-                    else if (reponse == 0)
-                    {
+                switch (statut_id) {
+
+                    case 0:
+                        erreur.style.color = "green";
+                        erreur.style.fontSize = "20px";
+                        erreur.innerHTML = "Veuillez vous connectez avec le mail suivant : " + mail.value + ".";
+                        break;
+                    case 1:
                         erreur.style.color = "red";
                         erreur.style.fontSize = "20px";
-                        erreur.innerHTML = "ERROR, le mail " + msg.messageAccount.email + " est déja utilisé";
-                    }
-                    else
-                        alert("ALERTE");*/
-
-
-                  switch (statut_id) {
-
-                        case 0:
-                            erreur.style.color = "green";
-                            erreur.style.fontSize = "20px";
-                            erreur.innerHTML = "Veuillez vous connectez avec le mail suivant : " + mail.value + ".";
-                            break;
-                      case 1:
-                          erreur.style.color = "red";
-                          erreur.style.fontSize = "20px";
-                          erreur.innerHTML = "Votre compte existe déjà ! ";
-                            break;
-                    }
-
+                        erreur.innerHTML = "Votre compte existe déjà ! ";
+                        break;
                 }
 
+            }
 
-            });
-       //
+
+        });
+        //
 
     }
 
@@ -310,10 +297,10 @@ function initialisation(choix) {
         };
     }
     else if (choix == 3) {
-        var latlng = new google.maps.LatLng(48.95867389954952, 2.548546097350723);
+        var latlng = new google.maps.LatLng(48.86075335917802, 2.338617480811113);
         var options = {
             center: latlng,
-            zoom: 16.8,
+            zoom: 12,
         };
     }
     else
@@ -337,16 +324,16 @@ function initialisation(choix) {
                 console.log(mark[0]);
 
                 for (let y = 0; y < mark[0].length; y++) {
-                    markers = new google.maps.Marker({
+                 /*   markers = new google.maps.Marker({
                         position: { lat: mark[0][y].lat, lng: mark[0][y].lng },
                         map: carte,
-                    });
+                    });*/
                     const lignes = new google.maps.Polyline({
                         path: mark[0],
                         geodesic: true,
                         strokeColor: "#FF0000",
                         strokeOpacity: 1.0,
-                        strokeWeight: 2,
+                        strokeWeight: 4,
                     });
                     lignes.setMap(carte);
                 }
@@ -356,10 +343,10 @@ function initialisation(choix) {
                 console.log(mark[1]);
 
                 for (let y = 0; y < mark[1].length; y++) {
-                    markers = new google.maps.Marker({
+                    /*markers = new google.maps.Marker({
                         position: { lat: mark[1][y].lat, lng: mark[1][y].lng },
                         map: carte,
-                    });
+                    });*/
                     const lignes = new google.maps.Polyline({
                         path: mark[1],
                         geodesic: true,
@@ -374,10 +361,10 @@ function initialisation(choix) {
                 console.log(mark[2]);
 
                 for (let y = 0; y < mark[1].length; y++) {
-                    markers = new google.maps.Marker({
+                   /* markers = new google.maps.Marker({
                         position: { lat: mark[2][y].lat, lng: mark[2][y].lng },
                         map: carte,
-                    });
+                    });*/
                     const lignes = new google.maps.Polyline({
                         path: mark[2],
                         geodesic: true,
--- assets/php/traitement.php
+++ assets/php/traitement.php
@@ -1,6 +1,110 @@
-<?php 
-    $mail=$_GET["email"];
-    $pass=$_GET["password"];
+<?php
 
-    echo $mail. $pass;
+$mail=$_GET["email"];
+$pass=$_GET["password"];
+
+$method=  $_GET["method"];
+
+
+
+
+//var_dump($obj[0]);
+if ($method == "SignIn") {
+    $file = '../js/accounts.json';
+    $data = file_get_contents($file);
+    $obj = json_decode($data);
+    $status_id = 0 ;
+    for ($i = 0; $i < sizeof($obj); $i++) {
+        if ($mail == $obj[$i]->email) {
+            $status_id = 0;
+            if ($pass == $obj[$i]->password) {
+                $status_id = 1;
+
+                break;
+            }
+        }
+
+    }
+    $myarray = array("messageAccount" => array("status_id" => $status_id, "email" => "$mail"));
+    echo json_encode($myarray);
+}
+
+//var_dump($method);
+
+      if ($method == "SignUp")
+      {
+
+          $data_results = file_get_contents('../js/accounts.json');
+          $tempArray = json_decode($data_results);
+          $status_id = 0 ;
+          for ($i = 0 ; $i < sizeof($tempArray) ; $i++)
+          {
+              if ($mail == $tempArray[$i]->email)
+              {
+                  if ($pass == $tempArray[$i]->password)
+                  {
+                      $status_id = 1 ;
+                      $myarray = array("messageAccount" => array("status_id" => $status_id, "email" => "$mail"));
+                      echo json_encode($myarray);
+                      break ;
+                  }
+              }
+      }
+
+
+          if ($status_id == 0)
+          {
+
+              $data_results = file_get_contents('../js/accounts.json');
+              $tempArray = json_decode($data_results);
+
+              $additionalArray = array("email"=>$mail,"password"=>"$pass");
+
+              $tempArray[] = $additionalArray ;
+              $jsonData = json_encode($tempArray);
+              file_put_contents('../js/accounts.json', $jsonData);
+              $myarray = array("messageAccount" => array("status_id" => $status_id, "email" => "$mail"));
+              echo json_encode($myarray);
+
+          }
+
+
+
+  }
+
+/*
+$additionalArray = array(
+    'id' => "is",
+    'title' => 'title',
+    'content' => 'title'
+);
+
+//open or read json data
+$data_results = file_get_contents('results.json');
+$tempArray = json_decode($data_results);
+
+//append additional json to json file
+
+$tempArray[] = $additionalArray ;
+$jsonData = json_encode($tempArray);
+
+file_put_contents('results.json', $jsonData);*/
+
+
+   // $tableau=array($mail,$pass);
+
+        $header="MIME-Version: 1-0\r\n";
+        $header.='From:"webminiprojet.com"<support@webminiprojet.com>'."\n";
+        $header.='Content-Type:text/html; charset="utf-8"'."\n";
+        $header.='Content-Transfer-Encoding: 8bit';
+         $msg="<html>
+               <head>
+               </head>
+               <body>
+                   <h1>Vérification !!!<h1/>
+               </body>
+               </html>";
+        mail($mail,"Vérification",$msg, $header);   //envoie du mail a l'adresse enregistrer
+
 ?>
+
